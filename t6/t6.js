4d33f0cda47371c96d3d304cf9ce8d107cbcdbad
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-08-22 11:44:13 +0300

    update

---
+++ t6/t6.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const kohde = document.querySelector('#target');
+
+const elokuvat = [];
+
+const lkm = prompt('Syötä elokuvien lkm');
+
+for (let i = 0; i < lkm; i++) {
+  const title = prompt('Syötä elokuvan nimi.');
+  const rating = prompt('Syötä arvio (1-5)');
+  const elokuva = {
+    title,
+    rating,
+  };
+
+  elokuvat.push(elokuva);
+}
+
+elokuvat.sort((a, b) => b.rating - a.rating);
+
+console.log(elokuvat);
+
+for (const elokuva of elokuvat) {
+  const html = `<tr>
+                  <td>${elokuva.title}</td>
+                  <td>${elokuva.rating}</td>
+                </tr>`;
+  kohde.insertAdjacentHTML('beforeend', html);
+}
